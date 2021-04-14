4f2285264aa85d6ff0e43bca49a71f7c009a2673
Kittanut T <tkittanut@gmail.com>
2021-04-14 18:02:45 +0700

    create settings component.

--- angular-homework/angular-chap-14-22/wip/homework-3-ui-search-tabs-tree/mock-vscode-preference/src/app/app-routing.module.ts
+++ angular-homework/angular-chap-14-22/wip/homework-3-ui-search-tabs-tree/mock-vscode-preference/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {path: '', redirectTo: '/settings-user', pathMatch: 'full'},
+  // {path: 'settings-user', component: UserComponent},
+  // {path: 'settings-workspace', component: WorkspaceComponent},
+  // {path: '**', component: UserComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-homework/angular-chap-14-22/wip/homework-3-ui-search-tabs-tree/mock-vscode-preference/src/app/app.module.ts
+++ angular-homework/angular-chap-14-22/wip/homework-3-ui-search-tabs-tree/mock-vscode-preference/src/app/app.module.ts
@@ -4,16 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import {BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    
+
     BrowserAnimationsModule,
   ],
   providers: [],
