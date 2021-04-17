4fad6dc72022b68acffd37f9a41684dbdbeba44b
Kittanut T <tkittanut@gmail.com>
2021-04-17 11:48:16 +0700

    add incomplete orders table.

--- angular-homework/angular-chap-23-27/homework-3-e-commerce/e-commerce/src/app/dashboard/incomplete-orders/incomplete-orders.component.ts
+++ angular-homework/angular-chap-23-27/homework-3-e-commerce/e-commerce/src/app/dashboard/incomplete-orders/incomplete-orders.component.ts
@@ -1,11 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface IncompleteOrdersElement {
+  item: string;
+  total: number;
+  count: number;
+}
+
+const ELEMENT_DATA: IncompleteOrdersElement[] = [
+  {item: 'Total unpaid orders (pending payment status)', total: 0, count: 2},
+  {item: 'Total not yet shipped orders', total: 0, count: 2},
+  {item: 'Total incomplete orders (pending order status)', total: 0, count: 2},
+];
+
 @Component({
   selector: 'app-incomplete-orders',
   templateUrl: './incomplete-orders.component.html',
   styleUrls: ['./incomplete-orders.component.css']
 })
 export class IncompleteOrdersComponent implements OnInit {
+  displayedColumns: string[] = ['item', 'total', 'count'];
+  dataSource = ELEMENT_DATA;
 
   constructor() { }
 
