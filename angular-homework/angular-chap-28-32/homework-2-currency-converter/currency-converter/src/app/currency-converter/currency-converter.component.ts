1dabafacf073437921b1f4ee28515a6a85262e3a
Kittanut T <tkittanut@gmail.com>
2021-04-19 11:39:26 +0700

    clear unused codes.

--- angular-homework/angular-chap-28-32/homework-2-currency-converter/currency-converter/src/app/currency-converter/currency-converter.component.ts
+++ angular-homework/angular-chap-28-32/homework-2-currency-converter/currency-converter/src/app/currency-converter/currency-converter.component.ts
@@ -10,7 +10,7 @@ import { ExchangeRatesService } from '../exchange-rates.service';
 export class CurrencyConverterComponent implements OnInit {
   @Input() title: string;
   form: FormGroup
-  posts: any;
+  cache: any;
 
   currencyList: string[] = ["AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", "AZN", "BAM", "BBD", "BDT", "BGN", "BHD", "BIF", "BMD", "BND", "BOB", "BRL", "BSD", "BTC", "BTN", "BWP", "BYN", "BYR", "BZD", "CAD", "CDF", "CHF", "CLF", "CLP", "CNY", "COP", "CRC", "CUC", "CUP", "CVE", "CZK", "DJF", "DKK", "DOP", "DZD", "EGP", "ERN", "ETB", "EUR", "FJD", "FKP", "GBP", "GEL", "GGP", "GHS", "GIP", "GMD", "GNF", "GTQ", "GYD", "HKD", "HNL", "HRK", "HTG", "HUF", "IDR", "ILS", "IMP", "INR", "IQD", "IRR", "ISK", "JEP", "JMD", "JOD", "JPY", "KES", "KGS", "KHR", "KMF", "KPW", "KRW", "KWD", "KYD", "KZT", "LAK", "LBP", "LKR", "LRD", "LSL", "LTL", "LVL", "LYD", "MAD", "MDL", "MGA", "MKD", "MMK", "MNT", "MOP", "MRO", "MUR", "MVR", "MWK", "MXN", "MYR", "MZN", "NAD", "NGN", "NIO", "NOK", "NPR", "NZD", "OMR", "PAB", "PEN", "PGK", "PHP", "PKR", "PLN", "PYG", "QAR", "RON", "RSD", "RUB", "RWF", "SAR", "SBD", "SCR", "SDG", "SEK", "SGD", "SHP", "SLL", "SOS", "SRD", "STD", "SVC", "SYP", "SZL", "THB", "TJS", "TMT", "TND", "TOP", "TRY", "TTD", "TWD", "TZS", "UAH", "UGX", "USD", "UYU", "UZS", "VEF", "VND", "VUV", "WST", "XAF", "XAG", "XAU", "XCD", "XDR", "XOF", "XPF", "YER", "ZAR", "ZMK", "ZMW", "ZWL"]; // country code (ISO 4217 format)
   baseCurrency:string;
@@ -44,7 +44,7 @@ export class CurrencyConverterComponent implements OnInit {
       // console.log(`result is ${result}`);
       console.log(`ping`);
       console.log(result);
-      this.posts = result as any;
+      this.cache = result as any;
     });
   }
 
@@ -54,10 +54,10 @@ export class CurrencyConverterComponent implements OnInit {
     console.log( JSON.stringify(this.form.value) );
     // SELECT USER INPUT: BASE CURRENCY: THEN FETCH EUR-BASERATE (eg. EUR-USD rate, EUR-THB rate, etc.)
     let baseMatch: string = this.form.value.baseCurrency;
-    this.basePairFxRate = Object.entries(this.posts)[4][1][baseMatch];
+    this.basePairFxRate = Object.entries(this.cache)[4][1][baseMatch];
     // SELECT USER INPUT: TARGET CURRENCY: THEN FETCH EUR-TARGETRATE (eg. EUR-USD rate, EUR-THB rate, etc.)
     let targetMatch: string = this.form.value.targetCurrency;
-    this.targetPairFxRate = Object.entries(this.posts)[4][1][targetMatch];
+    this.targetPairFxRate = Object.entries(this.cache)[4][1][targetMatch];
     // PRINT TO CONSOLE: CROSS CHECK EUR-BASERATE & EUR-TARGETRATE
     console.log(`EUR to ${baseMatch} is ${this.basePairFxRate}`);
     console.log(`EUR to ${targetMatch} is ${this.targetPairFxRate}`);
